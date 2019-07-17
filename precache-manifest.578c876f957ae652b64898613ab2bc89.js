3315b7cb81b0ca089f5ce8386b44d6f71a1a13f7
Nick Rashchuk <raschyk1993@gmail.com>
2019-07-18 00:12:01 +0300

    Updates

---
+++ precache-manifest.578c876f957ae652b64898613ab2bc89.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "15bb0d77a38a6ad292d1754b3013aa8f",
+    "url": "/TestAdyax/index.html"
+  },
+  {
+    "revision": "b2fc79977678784efd21",
+    "url": "/TestAdyax/static/css/main.01750455.chunk.css"
+  },
+  {
+    "revision": "fe511e1400168e62c875",
+    "url": "/TestAdyax/static/js/2.062c1c39.chunk.js"
+  },
+  {
+    "revision": "b2fc79977678784efd21",
+    "url": "/TestAdyax/static/js/main.e9bb3fe3.chunk.js"
+  },
+  {
+    "revision": "f4aa2143fddefab652dc",
+    "url": "/TestAdyax/static/js/runtime~main.8e1b535a.js"
+  },
+  {
+    "revision": "659063c8936183ed92b08510133ed79b",
+    "url": "/TestAdyax/static/media/Avalon_Bold.659063c8.ttf"
+  },
+  {
+    "revision": "d04c5ee628944b7b772d3d70f37e93d8",
+    "url": "/TestAdyax/static/media/bitmap.d04c5ee6.png"
+  }
+]);
